232bec02f60278ba23206852e20549d7aedb5f32
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-08-10 05:57:07 +0200

    Create a component and fetch API

---
+++ src/components/DisplayQuote.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+function DisplayQuote() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=faith',
+          {
+            headers: {
+              'X-Api-Key': '5IxClj7l7PEht3AA8nJbiQ==CjzPZ7VSPNDaBiwc',
+            },
+          });
+        if (res.ok) {
+          const result = await res.json();
+          setData(result);
+        } else {
+          setError('Failed to fetch quote');
+        }
+      } catch (error) {
+        setError('Failed to fetch quote');
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      { error ? 'Failed to fetch quote' : null }
+      {isLoading
+        ? (<p className="loader">Loading...</p>)
+        : (
+          <p className="quote">
+            {data.map((item) => (
+              item.quote
+            ))}
+          </p>
+        )}
+    </div>
+  );
+}
+
+export default DisplayQuote;
