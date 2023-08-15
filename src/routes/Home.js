c73b34851c28102dc3329ee77ff90cf0b467c795
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-08-15 06:23:26 +0200

    Add style to the header

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import loremIpsum from 'react-lorem-ipsum';
+import LoremIpsum from 'react-lorem-ipsum';
 import '../styles/Home.css';
 
 function Home() {
@@ -6,11 +6,7 @@ function Home() {
     <div>
       <h2 style={{ margin: '60px 140px' }}>Welcome to our page!</h2>
       <div className="text-wrapper">
-        {loremIpsum({ avgSentencesPerParagraph: 10, random: true, p: 2 }).map((text) => (
-          <div className="text" key={text}>
-            {text}
-          </div>
-        ))}
+        <LoremIpsum avgSentencesPerParagraph={10} p={2} />
       </div>
     </div>
   );
