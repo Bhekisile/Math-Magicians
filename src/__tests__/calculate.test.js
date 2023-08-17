b084b526b389b78d4ed3f4e704f2fce30b49d996
Akorede Emiola <emiolasaheed1@gmail.com>
2023-08-17 10:06:36 +0100

    Update test message

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -1,6 +1,6 @@
 import * as Calculate from '../logic/calculate';
 
-test('checking behavior of isNumber related to regex matching.', () => {
+test('checking the behavior of isNumber related to regex matching.', () => {
   const output = Calculate.isNumber('7');
   expect(output).toBe(true);
 });
